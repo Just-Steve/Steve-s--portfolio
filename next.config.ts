55b440477ca642d757fb260faccd97e754f8da46
Just-Steve <stevegichui03@gmail.com>
2026-01-17 14:50:47 +0300

    Update next.config.ts: replace images.domains with remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,29 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
-    domains: ["assets.aceternity.com"], 
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "assets.aceternity.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "martinfowler.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "encrypted-tbn0.gstatic.com",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
