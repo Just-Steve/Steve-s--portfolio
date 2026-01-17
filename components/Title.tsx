cef776ab5f29bd7067058f0def90e4807d3e4e97
Just-Steve <stevegichui03@gmail.com>
2026-01-18 02:32:28 +0300

    Fix Title component typing and styling

--- components/Title.tsx
+++ components/Title.tsx
@@ -1,16 +1,23 @@
 import React from 'react'
-import { Cover } from './ui/Cover';
+import { Cover } from './ui/Cover'
 
 interface TitleProps {
-    text?: string;
-    className?: string;
-    coverText?: string;
+  text?: string
+  coverText?: string
+  className?: string
 }
 
-const Title = ({text, coverText}: TitleProps) => {
+const Title = ({ text, coverText, className = '' }: TitleProps) => {
   return (
-    <h1 className="uppercase text-center text-xl md:text-2xl lg:text-4xl font-semibold max-w-7xl mx-auto mt-6 relative z-20 py-6 bg-clip-text text-transparent bg-gradient-to-b  from-neutral-800 via-white to-white flex gap-2 items-center justify-center">
-        {text}<Cover>{coverText}</Cover>
+    <h1
+      className={`uppercase text-center text-xl md:text-2xl lg:text-4xl font-semibold
+      max-w-7xl mx-auto mt-6 relative z-20 py-6
+      bg-clip-text text-transparent bg-gradient-to-b
+      from-neutral-800 via-white to-white
+      flex gap-2 items-center justify-center ${className}`}
+    >
+      {text}
+      {coverText && <Cover>{coverText}</Cover>}
     </h1>
   )
 }
